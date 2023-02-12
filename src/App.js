8202c0f61a74c8c710d5f51920f92e581ccd72ab
Bilal Ahad <engr.bilalahad2001@gmail.com>
2023-02-12 17:16:17 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -19,17 +19,17 @@ function App() {
 
  }
   return (
-    <div className="App h-screen grid place-items-center bg-gray-200 ">
+    <div className="App h-[100vh] grid sm:place-items-center bg-gray-200 ">
      <form action="" className="bg-gray-800 shadow-2xl border-t-8 border-blue-800 w-96 grid gap-4 rounded-lg text-gray-500 max-w-5xl p-8">
      <h1 className=" text-white font-bold text-2xl mb-5 font-serif ">Register</h1>
      <input type="text" value={data.username} placeholder="Name" name="username" id="" className=" text-sm rounded-md border-2 h-9 border-gray-400" onChange={handle}/>
      <input type="email" value={data.email} placeholder="Email" name="email" id="" className=" text-sm rounded-md border-2 h-9 border-gray-400" onChange={handle} />
      <input type="passward" value={data.passward} placeholder="passward" name="passward" id="" className="text-sm rounded-md border-2 h-9 border-gray-400" onChange={handle} />
-     <input type="passward" value={data.conformpassward} placeholder="conform passward" name="conform passward" id="" className="text-sm rounded-md border-2 h-9 border-gray-400" onChange={handle} />
+     <input type="passward" value={data.conformpassward} placeholder="conformpassward" name="conform passward" id="" className="text-sm rounded-md border-2 h-9 border-gray-400" onChange={handle} />
      <button className="bg-blue-900  hover:brightness-75 rounded-lg  w-32 h-9 text-white">
         Create Account
      </button>
-     <p className='italic'>{`My name is ${data.username}  and my email is ${data.email}`}</p>
+     <p className='italic '>{`My name is ${data.username}  and my email is ${data.email}`}</p>
      </form>
     </div>
   );
