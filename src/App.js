303ef7f45d7b6f488fd583ea79ff9fe4d3794bbd
Bilal Ahad <engr.bilalahad2001@gmail.com>
2023-02-12 17:22:14 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
 
  }
   return (
-    <div className="App h-[100vh] grid sm:place-items-center bg-gray-200 ">
-     <form action="" className="bg-gray-800 shadow-2xl border-t-8 border-blue-800 w-96 grid gap-4 rounded-lg text-gray-500 max-w-5xl p-8">
+    <div className="App h-[100vh] grid sm:place-items-center justify-self-center bg-gray-200 ">
+     <form action="" className="bg-gray-800 shadow-2xl border-t-8 border-blue-800 w-96 grid gap-4 rounded-lg text-gray-500  max-w-5xl p-8">
      <h1 className=" text-white font-bold text-2xl mb-5 font-serif ">Register</h1>
      <input type="text" value={data.username} placeholder="Name" name="username" id="" className=" text-sm rounded-md border-2 h-9 border-gray-400" onChange={handle}/>
      <input type="email" value={data.email} placeholder="Email" name="email" id="" className=" text-sm rounded-md border-2 h-9 border-gray-400" onChange={handle} />
